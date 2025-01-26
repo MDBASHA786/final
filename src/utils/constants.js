a885f63a7febeed8eba7373eea0e2b6b06fd5555
MDBASHA786 <130646269+MDBASHA786@users.noreply.github.com>
2025-01-26 11:24:59 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,122 @@
+export const META_EVENTS = [
+  {
+    title: "TECHNICAL EVENTS",
+    desc: "Unlock your potential and ignite your curiosity",
+    events: [
+      {
+        name: "PaperCres  ",
+        imgURL: "/imgs/tech/event1.webp",
+        time: "10:30 AM",
+        type: "tech",
+        path: "/event/PaperCres ",
+      },
+      {
+        name: "CodeRoulette",
+        imgURL: "/imgs/tech/event2.webp",
+        time: "10:30AM",
+        type: "tech",
+        path: "/event/CodeRoulette",
+      },
+      {
+        name: "Interface",
+        imgURL: "/imgs/tech/event4.webp",
+        time: "10:30AM",
+        type: "tech",
+        path: "/event/Interface",
+      },
+      {
+        name: "TechQuest",
+        imgURL: "/imgs/tech/event5.webp",
+        time: "10:50AM",
+        type: "tech",
+        path: "/event/TechQuest",
+      },
+    ],
+  },
+  {
+    title: "NON TECH EVENTS",
+    desc: "Embrace discovery, every step brings new horizons",
+    events: [
+        {
+          name: "GameVerse",
+          imgURL: "/imgs/nontech/event1.webp",
+          time: "10:30 AM",
+          type: "non-tech",
+          path: "/event/GameVerse",
+        },
+        {
+          name: "BoxCricket",
+          imgURL: "/imgs/nontech/event2.webp",
+          time: "10:30AM",
+          type: "non-tech",
+          path: "/event/BoxCricket",
+        },
+        {
+          name: "ESports",
+          imgURL: "/imgs/nontech/event3.webp",
+          time: "10:30AM",
+          type: "non-tech",
+          path: "/event/ESports",
+        },
+        {
+          name: "BioScope",
+          imgURL: "/imgs/nontech/event4.webp",
+          time: "10:30AM",
+          type: "non-tech",
+          path: "/event/BioScope",
+        },
+      ]
+  }
+];
+
+export const SPONSERS = [
+  // { name: "Meriba Paneer", imgURL: "/imgs/sponsors/sp1.webp" },
+  // { name: "Upkar Foundation", imgURL: "/imgs/sponsors/sp4.webp" },
+  // { name: "Trust", imgURL: "/imgs/sponsors/sp5.webp" },
+];
+
+export const CONTACT_INFO = [
+  {
+    title: "Faculty Coordinators",
+    role: "staff",
+    data: [
+      { name: "Dr. Nabeena Ameen" },
+      { name: "Dr. Nabeena Ameen" },
+    ],
+  },
+  {
+    title: "Event Coordinators",
+    role: "student",
+    data: [
+      { name: "Mohamed Basha S" },
+      { name: "Aafreen Marzuqa  A " },
+      { name: "Mohamed Faizal S H "},
+      { name: "Rakshana T" },
+    ],
+  },
+  {
+    title: "HOD, IT",
+    role: "hod",
+    data: [{ name: "Dr . N . Prakash "}],
+  },
+  {
+    title: "Help Desk",
+    role: "help",
+    data: [
+      { name: "Thamim M", phone: "9384466146" },
+      { name: "Mohamed Basha", phone: "9363396122" },
+    ],
+  },
+];
+
+export const contentData = {
+  symposium: {
+    title: "About Symposium",
+    text: "ICON, a National-level symposium, is meticulously organized by the professional society 'Society of Information Technologists (SIT),' as a platform to elevate the technical skills of students. This symposium, hosted by the department, serves as a dynamic arena for knowledge exchange and skill enhancement, fostering a vibrant community of Information Technology enthusiasts.Join us at ICON to engage with cutting-edge insights, network with industry experts, and empower your journey in the ever-evolving field of technology."
+  },
+  college: {
+    title: "About Department",
+    img: "/imgs/grp.jpeg",
+    text: "The department was started in 1999 and has been offering B.Tech.-Information Technology since 1999, M.Tech.-Information Technology since 2010 and Ph.D since 2009. It has highly qualified, experienced and dedicated faculty members specialized in various areas like Artificial Intelligence, Deep Learning, Machine Learning, IoT, IIoT, Data Science, Cyber Security, Cloud Computing, Mobile Computing & Networks, Computer Networks, Ad hoc Networks and Software Engineering."
+  }
+};
