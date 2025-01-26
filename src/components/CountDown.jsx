7d7204b6daccc610f62d9fdc29795db32458f208
MDBASHA786 <mohamedbasha2207@gmail.com>
2025-01-26 18:02:29 +0530

    Your commit message

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-// function CountDown() {
+function CountDown() {
 //     //timer code 
 //     {const [countDown, setCountDown] = useState({
 //         days: 0,
